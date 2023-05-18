9b4bf732196056912a1d6677dccc675934cbde76
soongon <soongon@gmail.com>
2023-05-18 09:51:24 +0900

    initial commit

--- src/main/java/kr/or/ssis/todo/dao/TodoDao.java
+++ src/main/java/kr/or/ssis/todo/dao/TodoDao.java
@@ -1,18 +1,15 @@
 package kr.or.ssis.todo.dao;
 
 import kr.or.ssis.todo.model.Todo;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.Arrays;
 import java.util.List;
 
 @Repository
-public class TodoDao {
+@Mapper
+public interface TodoDao {
 
-    public List<Todo> selectAllTodos() {
-        return Arrays.asList(
-                new Todo(1, "청소하기", false),
-                new Todo(2, "음악감상하기", false)
-        );
-    }
+    List<Todo> selectAllTodos();
 }
